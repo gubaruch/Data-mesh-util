cea132051c99bdfec7f02a1efdce5f7411c4161f
Ian Meyers <meyersi@amazon.com>
2021-07-06 13:13:29 +0100

    adding s3 bucket policy push

--- src/data_mesh_util/DataMeshManager.py
+++ src/data_mesh_util/DataMeshManager.py
@@ -3,7 +3,7 @@ import pystache
 import os
 import sys
 import json
-import urllib.parse
+import shortuuid
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "resource"))
 
@@ -14,6 +14,8 @@ DEFAULT_TAGS = [
     },
 ]
 DATA_MESH_MANAGER_ROLENAME = 'DataMeshManager'
+DATA_MESH_ADMIN_PRODUCER_ROLENAME = 'DataMeshAdminProducer'
+DATA_MESH_ADMIN_CONSUMER_ROLENAME = 'DataMeshAdminConsumer'
 DATA_MESH_PRODUCER_ROLENAME = 'DataMeshProducer'
 DATA_MESH_CONSUMER_ROLENAME = 'DataMeshConsumer'
 DATA_MESH_IAM_PATH = '/AwsDataMesh/'
@@ -22,6 +24,8 @@ PRODUCER_S3_POLICY_NAME = 'GetPutS3BucketPolicy'
 
 class DataMeshManager:
     _data_mesh_account_id = None
+    _data_producer_account_id = None
+    _data_consumer_account_id = None
     _data_mesh_manager_role_arn = None
     _iam_client = None
     _sts_client = None
@@ -30,7 +34,6 @@ class DataMeshManager:
     def __init__(self):
         self._iam_client = boto3.client('iam')
         self._sts_client = boto3.client('sts')
-        self._data_mesh_account_id = self._sts_client.get_caller_identity().get('Account')
         self._renderer = pystache.Renderer()
 
     def _generate_policy(self, template_file: str, config: dict):
@@ -53,13 +56,25 @@ class DataMeshManager:
             ]
         }
 
-    def _create_role_and_attach_policy(self, policy_name: str, policy_desc: str, policy_template: str,
-                                       role_name: str, role_desc: str, config: dict):
+    def _validate_config(self, config: dict):
+        if config is None:
+            config = {}
+
         # add the data mesh account to the config if it isn't provided
         if "data_mesh_account_id" not in config:
             config["data_mesh_account_id"] = self._data_mesh_account_id
 
-        # create an IAM Policy that will allow the role to attach future policies to itself
+        if "producer_account_id" not in config:
+            config["producer_account_id"] = self._data_producer_account_id
+
+        if "consumer_account_id" not in config:
+            config["consumer_account_id"] = self._data_consumer_account_id
+
+    def _create_role_and_attach_policy(self, policy_name: str, policy_desc: str, policy_template: str,
+                                       role_name: str, role_desc: str, account_id: str, config: dict = None):
+        self._validate_config(config)
+
+        # create an IAM Policy from the template
         policy_doc = self._generate_policy(policy_template, config)
 
         try:
@@ -72,7 +87,7 @@ class DataMeshManager:
             )
             policy_arn = response.get('Policy').get('Arn')
         except self._iam_client.exceptions.EntityAlreadyExistsException:
-            policy_arn = "arn:aws:iam::%s:policy%s%s" % (self._data_mesh_account_id, DATA_MESH_IAM_PATH, policy_name)
+            policy_arn = "arn:aws:iam::%s:policy%s%s" % (account_id, DATA_MESH_IAM_PATH, policy_name)
 
         role_arn = None
         try:
@@ -81,7 +96,7 @@ class DataMeshManager:
                 Path=DATA_MESH_IAM_PATH,
                 RoleName=role_name,
                 AssumeRolePolicyDocument=json.dumps(
-                    self._get_assume_role_doc(principal="arn:aws:iam::%s:root" % self._data_mesh_account_id)),
+                    self._get_assume_role_doc(principal="arn:aws:iam::%s:root" % account_id)),
                 Description=role_desc,
                 Tags=DEFAULT_TAGS
             )
@@ -105,23 +120,26 @@ class DataMeshManager:
             policy_desc='Initial IAM Role enabling the Data Mesh Manager Policy to create future Resource Policies',
             policy_template="data_mesh_setup_iam_policy.pystache",
             role_name=DATA_MESH_MANAGER_ROLENAME,
-            role_desc='Role to be used for all Data Mesh Management functionality')
+            role_desc='Role to be used for all Data Mesh Management functionality',
+            account_id=self._data_mesh_account_id)
 
     def _create_producer_role(self):
         return self._create_role_and_attach_policy(
             policy_name='DataMeshProducerPolicy',
             policy_desc='IAM Role enabling Accounts to become Data Producers',
             policy_template="producer_policy.pystache",
-            role_name=DATA_MESH_PRODUCER_ROLENAME,
-            role_desc='Role to be used for all Data Mesh Producer Accounts')
+            role_name=DATA_MESH_ADMIN_PRODUCER_ROLENAME,
+            role_desc='Role to be used for all Data Mesh Producer Accounts',
+            account_id=self._data_mesh_account_id)
 
     def _create_consumer_role(self):
         return self._create_role_and_attach_policy(
             policy_name='DataMeshConsumerPolicy',
             policy_desc='IAM Role enabling Accounts to become Data Consumers',
             policy_template="consumer_policy.pystache",
-            role_name=DATA_MESH_CONSUMER_ROLENAME,
-            role_desc='Role to be used for all Data Mesh Consumer Accounts')
+            role_name=DATA_MESH_ADMIN_CONSUMER_ROLENAME,
+            role_desc='Role to be used for all Data Mesh Consumer Accounts',
+            account_id=self._data_mesh_account_id)
 
     def initialize_mesh_account(self):
         '''
@@ -129,6 +147,8 @@ class DataMeshManager:
         of the Data Mesh Account. Creates IAM Roles & Policies for the DataMeshManager, DataProducer, and DataConsumer
         :return:
         '''
+        self._data_mesh_account_id = self._sts_client.get_caller_identity().get('Account')
+
         # create a new IAM role in the Data Mesh Account to be used for future grants
         self._create_data_mesh_manager_role()
 
@@ -138,6 +158,8 @@ class DataMeshManager:
         # create the consumer role
         consumer_role = self._create_consumer_role()
 
+        return (self._data_mesh_manager_role_arn, producer_role, consumer_role)
+
     def initialize_producer_account(self, s3_bucket: str):
         '''
         Sets up an AWS Account to act as a Data Provider into the central Data Mesh Account. This method should be invoked
@@ -145,13 +167,22 @@ class DataMeshManager:
         Requires at least 1 S3 Bucket Policy be enabled for future grants.
         :return:
         '''
+        self._data_producer_account_id = self._sts_client.get_caller_identity().get('Account')
         return self._create_role_and_attach_policy(
             policy_name=PRODUCER_S3_POLICY_NAME,
             policy_desc='IAM Policy enabling Accounts to get and put restricted S3 Bucket Policies',
             policy_template="producer_update_bucket_policy.pystache",
             role_name=DATA_MESH_PRODUCER_ROLENAME,
             role_desc='Role to be used to update S3 Bucket Policies for crawling by Data Mesh Account',
-            config={"bucket": s3_bucket})
+            config={"bucket": s3_bucket},
+            account_id=self._data_producer_account_id)
+
+    def _validate_correct_account(self, role_must_exist: str):
+        try:
+            self._iam_client.get_role(RoleName=role_must_exist)
+            return True
+        except self._iam_client.exceptions.NoSuchEntityException:
+            return False
 
     def enable_future_bucket_sharing(self, s3_bucket: str):
         '''
@@ -160,9 +191,15 @@ class DataMeshManager:
         :param s3_bucket:
         :return:
         '''
+        self._data_producer_account_id = self._sts_client.get_caller_identity().get('Account')
+
+        # validate that we are being run within the correct account
+        if self._validate_correct_account(DATA_MESH_PRODUCER_ROLENAME) is False:
+            raise Exception("Function should be run in the Data Domain Producer Account")
+
         # get the producer policy
-        current_account = self._sts_client.get_caller_identity().get('Account')
-        arn = "arn:aws:iam::%s:policy%s%s" % (current_account, DATA_MESH_IAM_PATH, PRODUCER_S3_POLICY_NAME)
+        arn = "arn:aws:iam::%s:policy%s%s" % (
+            self._data_producer_account_id, DATA_MESH_IAM_PATH, PRODUCER_S3_POLICY_NAME)
         policy_version = self._iam_client.get_policy(PolicyArn=arn).get('Policy').get('DefaultVersionId')
         policy_doc = self._iam_client.get_policy_version(PolicyArn=arn, VersionId=policy_version).get(
             'PolicyVersion').get(
@@ -176,7 +213,50 @@ class DataMeshManager:
         if bucket_arn not in resources:
             resources.append(bucket_arn)
             policy_doc.get('Statement')[0]['Resource'] = resources
-            self._iam_client.create_policy_version(PolicyArn=arn, PolicyDocument=json.dumps(policy_doc), SetAsDefault=True)
+            self._iam_client.create_policy_version(PolicyArn=arn, PolicyDocument=json.dumps(policy_doc),
+                                                   SetAsDefault=True)
+
+    def grant_data_mesh_account_to_s3_bucket(self, s3_bucket: str, data_mesh_producer_role_arn: str):
+        self._data_producer_account_id = self._sts_client.get_caller_identity().get('Account')
+
+        # validate that we are being run within the correct account
+        if self._validate_correct_account(DATA_MESH_PRODUCER_ROLENAME) is False:
+            raise Exception("Function should be run in the Data Domain Producer Account")
+
+        s3_client = boto3.client('s3')
+        get_bucket_policy_response = None
+        try:
+            get_bucket_policy_response = s3_client.get_bucket_policy(Bucket=s3_bucket, ExpectedBucketOwner=self._data_producer_account_id)
+        except s3_client.exceptions.from_code('NoSuchBucketPolicy'):
+            pass
+
+        # generate a new statement for the target bucket policy
+        conf = {"data_mesh_role_arn": data_mesh_producer_role_arn, "bucket": s3_bucket}
+        statement = json.loads(self._generate_policy(template_file="producer_bucket_policy.pystache", config=conf))
+
+        bucket_policy = None
+        if get_bucket_policy_response is None or get_bucket_policy_response.get('Policy') is None:
+            bucket_policy = {
+                "Id": "Policy%s" % shortuuid.uuid(),
+                "Version":  "2012-10-17",
+                "Statement": [
+                    statement
+                ]
+            }
+        else:
+            bucket_policy = get_bucket_policy_response.get('Policy')
+
+            # add a statement that allows the data mesh admin producer read-only access
+            bucket_policy.get('Statement').append(statement)
+
+        print(bucket_policy)
+
+        s3_client.put_bucket_policy(
+            Bucket=s3_bucket,
+            ConfirmRemoveSelfBucketAccess=False,
+            Policy=json.dumps(bucket_policy),
+            ExpectedBucketOwner=self._data_producer_account_id
+        )
 
     def initialize_consumer_account(self):
         pass
--- test/producer_account_tests.py
+++ test/producer_account_tests.py
@@ -4,11 +4,16 @@ import warnings
 
 warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
 
+
 class DataMeshProducerAccountTests(unittest.TestCase):
     mgr = dmu.DataMeshManager()
 
     def test_producer_iam_role(self):
         self.mgr.initialize_producer_account(s3_bucket="org-1-data/tpcds")
 
-    def test_enable_bucket_access(self):
+    def test_enable_future_bucket_access(self):
         self.mgr.enable_future_bucket_sharing("blah/blah")
+
+    def test_grant_bucket_access(self):
+        self.mgr.grant_data_mesh_account_to_s3_bucket(s3_bucket="org-1-data",
+                                                      data_mesh_producer_role_arn="arn:aws:iam::887210671223:role/AwsDataMesh/DataMeshAdminProducer")
