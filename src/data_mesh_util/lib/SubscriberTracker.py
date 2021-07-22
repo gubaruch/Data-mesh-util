50a2bc2f5f82e5e309c15e28d98b7d75062d37c8
Ian Meyers <meyersi@amazon.com>
2021-07-22 14:32:18 +0100

    first cut of a status updattttte

--- src/data_mesh_util/lib/SubscriberTracker.py
+++ src/data_mesh_util/lib/SubscriberTracker.py
@@ -1,18 +1,33 @@
 from constants import *
 import boto3
+from boto3.dynamodb.conditions import Attr, Or, And
+
+STATUS_ACTIVE = 'Active'
+STATUS_DENIED = 'Denied'
+STATUS_PENDING = 'Pending'
+STATUS_DELETED = 'Deleted'
+SUBSCRIPTION_ID = 'SubscriptionId'
+OWNER_PRINCIPAL = 'OwnerPrincipal'
+SUBSCRIBER_PRINCIPAL = 'SubscriberPrincipal'
+STATUS = 'Status'
 
 
 class SubscriberTracker:
     _dynamo_client = None
+    _dynamo_resource = None
     _table_info = None
+    _table = None
 
     def __init__(self, region_name: str):
         self._dynamo_client = boto3.client('dynamodb', region_name=region_name)
+        self._dynamo_resource = boto3.resource('dynamodb', region_name=region_name)
 
         self._table_info = self._init_table()
 
     def _init_table(self):
-        def _extract_format(t):
+        t = None
+
+        def _extract_format():
             return {
                 'Table': t.get('TableArn'),
                 'Stream': t.get('LatestStreamArn')
@@ -23,25 +38,25 @@ class SubscriberTracker:
                 TableName=SUBSCRIPTIONS_TRACKER_TABLE,
                 AttributeDefinitions=[
                     {
-                        'AttributeName': 'SubscriptionId',
+                        'AttributeName': SUBSCRIPTION_ID,
                         'AttributeType': 'S'
                     },
                     {
-                        'AttributeName': 'SubscriberPrincipal',
+                        'AttributeName': SUBSCRIBER_PRINCIPAL,
                         'AttributeType': 'S'
                     },
                     {
-                        'AttributeName': 'OwnerPrincipal',
+                        'AttributeName': OWNER_PRINCIPAL,
                         'AttributeType': 'S'
                     },
                     {
-                        'AttributeName': 'Status',
+                        'AttributeName': STATUS,
                         'AttributeType': 'S'
                     }
                 ],
                 KeySchema=[
                     {
-                        'AttributeName': 'SubscriptionId',
+                        'AttributeName': SUBSCRIPTION_ID,
                         'KeyType': 'HASH'
                     }
                 ],
@@ -50,11 +65,11 @@ class SubscriberTracker:
                         'IndexName': "%s-%s" % (SUBSCRIPTIONS_TRACKER_TABLE, 'Owner'),
                         'KeySchema': [
                             {
-                                'AttributeName': 'OwnerPrincipal',
+                                'AttributeName': OWNER_PRINCIPAL,
                                 'KeyType': 'HASH',
                             },
                             {
-                                'AttributeName': 'Status',
+                                'AttributeName': STATUS,
                                 'KeyType': 'RANGE',
                             }
                         ],
@@ -66,11 +81,11 @@ class SubscriberTracker:
                         'IndexName': "%s-%s" % (SUBSCRIPTIONS_TRACKER_TABLE, 'Subscriber'),
                         'KeySchema': [
                             {
-                                'AttributeName': 'SubscriberPrincipal',
+                                'AttributeName': SUBSCRIBER_PRINCIPAL,
                                 'KeyType': 'HASH',
                             },
                             {
-                                'AttributeName': 'Status',
+                                'AttributeName': STATUS,
                                 'KeyType': 'RANGE',
                             }
                         ],
@@ -87,14 +102,58 @@ class SubscriberTracker:
                 Tags=DEFAULT_TAGS
             )
 
-            return _extract_format(response.get('TableDescription'))
+            t = response.get('TableDescription')
         except (self._dynamo_client.exceptions.from_code('AlreadyExistsException'),
                 self._dynamo_client.exceptions.from_code('ResourceInUseException')):
             response = self._dynamo_client.describe_table(
                 TableName=SUBSCRIPTIONS_TRACKER_TABLE
             )
 
-            return _extract_format(response.get('Table'))
+            t = response.get('Table')
+
+        self._table = self._dynamo_resource.Table(SUBSCRIPTIONS_TRACKER_TABLE)
+
+        return _extract_format()
 
     def get_endpoints(self):
         return self._table_info
+
+    def update_status(self, subscription_id: str, status: str):
+        '''
+        Updates the status of a subscription. Valid transitions are:
+        PENDING->ACTIVE
+        PENDING->DENIED
+        DENIED->ACTIVE
+        ACTIVE->DELETED
+
+        :param subscription_id:
+        :param status:
+        :return:
+        '''
+        # build the map of proposed status to allowed status
+        if status == STATUS_ACTIVE:
+            expected = Or(Attr(STATUS).eq(STATUS_PENDING), Attr(STATUS).eq(STATUS_DENIED))
+        elif status == STATUS_DENIED:
+            expected = Attr(STATUS).eq(STATUS_PENDING)
+        elif status == STATUS_DELETED:
+            expected = Attr(STATUS).eq(STATUS_ACTIVE)
+
+        response = self._table.update_item(
+            Key={
+                SUBSCRIPTION_ID: subscription_id
+            },
+            UpdateExpression="set :status = #status",
+            ExpressionAttributeNames={
+                ":status": STATUS
+            },
+            ExpressionAttributeValues={
+                "#status": status
+            },
+            ConditionExpression=expected
+        )
+
+        if response is None or response.get('ConsumedCapacity') is None or response.get('ConsumedCapacity').get(
+                'CapacityUnits') == 0:
+            raise Exception("Invalid State Transition")
+        else:
+            return True
