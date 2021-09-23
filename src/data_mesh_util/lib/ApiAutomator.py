b4040598f4e62f75baca8a90d13d4ab19508d3be
Ian Meyers <meyersi@amazon.com>
2021-09-23 11:08:06 +0100

    Adding handling for case of where bucket policy has already been updated

---
+++ src/data_mesh_util/lib/ApiAutomator.py
@@ -0,0 +1,102 @@
+import sys
+import logging
+
+import shortuuid
+
+from data_mesh_util.lib.constants import *
+import json
+import data_mesh_util.lib.utils as utils
+
+
+class ApiAutomator:
+    _session = None
+    _logger = None
+    _region = None
+    _logger = logging.getLogger("ApiAutomator")
+    # make sure we always log to standard out
+    _logger.addHandler(logging.StreamHandler(sys.stdout))
+    _clients = {}
+
+    def __init__(self, session, log_level: str = "INFO"):
+        self._session = session
+        self._logger.setLevel(log_level)
+
+    def _get_client(self, client):
+        client = self._clients.get(client)
+
+        if client is None:
+            client = self._session.client(client)
+            self._clients[client] = client
+
+        return client
+
+    def _get_bucket_name(self, bucket_value):
+        if 's3://' in bucket_value:
+            return bucket_value.split('/')[2]
+        else:
+            return bucket_value
+
+    def _transform_bucket_policy(self, bucket_policy: dict, principal_account: str,
+                                 bucket_name: str) -> dict:
+        use_bucket_name = self._get_bucket_name(bucket_name)
+        policy_sid = f"{BUCKET_POLICY_STATEMENT_SID}-{use_bucket_name}"
+
+        # generate a new bucket policy from the template
+        base_policy = json.loads(utils.generate_policy(template_file='producer_bucket_policy.pystache', config={
+            'account_id': principal_account,
+            'bucket': use_bucket_name,
+            'sid': policy_sid
+        }))
+
+        if bucket_policy is None:
+            generated_policy = {
+                "Version": "2012-10-17",
+                "Id": shortuuid.uuid(),
+                "Statement": [
+                    base_policy
+                ]
+            }
+            self._logger.info(
+                f"Creation new S3 Bucket policy enabling Data Mesh LakeFormation Service Role access for {principal_account}")
+            self._logger.info(f"Creating new Bucket Policy for {bucket_name}")
+            return generated_policy
+        else:
+            # we already have a bucket policy, so determine if there is already a data mesh grant created for this bucket
+            statements = bucket_policy.get('Statement')
+            data_mesh_statement_index = -1
+
+            for i, s in enumerate(statements):
+                if s.get('Sid') == policy_sid:
+                    data_mesh_statement_index = i
+                    break
+
+            if data_mesh_statement_index == -1:
+                # there was not a previously created data mesh auth statement, so add it to the end
+                statements.append(base_policy)
+                self._logger.info(
+                    f"Adding new Data Mesh LakeFormation Service Role statement for {principal_account} to existing Bucket Policy")
+            else:
+                # we already have a data mesh auth statement, so check if the principal is already there first
+                statement = statements[data_mesh_statement_index]
+                set_principal = f"arn:aws:iam::{principal_account}:role/aws-service-role/lakeformation.amazonaws.com/AWSServiceRoleForLakeFormationDataAccess"
+                if set_principal not in statement.get('Principal').get('AWS'):
+                    statement.get('Principal').get('AWS').append(set_principal)
+                    statements[data_mesh_statement_index] = statement
+                    self._logger.info(
+                        f"Adding principal {principal_account} to existing Data Mesh LakeFormation Service Role statement")
+
+                    bucket_policy['Statement'] = statements
+                else:
+                    self._logger.info("Not modifying bucket policy as principal has already been added")
+
+            return bucket_policy
+
+    def add_bucket_policy_entry(self, principal_account: str, bucket_name: str):
+        s3_client = self._get_client('s3')
+
+        bucket_policy = s3_client.get_bucket_policy(Bucket=bucket_name)
+
+        new_policy = self._transform_bucket_policy(bucket_policy=bucket_policy, principal_account=principal_account,
+                                                   bucket_name=bucket_name)
+
+        s3_client.put_bucket_policy(Bucket=bucket_name, policy=new_policy)
---
+++ test/test_api_automator.py
@@ -0,0 +1,121 @@
+import unittest
+import sys
+import os
+import warnings
+import boto3
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src/resource"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src/lib"))
+
+from data_mesh_util.lib.ApiAutomator import *
+
+warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
+
+MESH_ACCOUNT = '887210671223'
+CONSUMER_ACCOUNT = '206160724517'
+PRODUCER_ACCOUNT = '600214582022'
+
+
+class ApiAutomatorTests(unittest.TestCase):
+    _session = boto3.session.Session()
+    _automator = ApiAutomator(session=_session, log_level='INFO')
+    _proofs = None
+
+    def setUp(self) -> None:
+        with open('s3_bucket_policy_proofs.json', 'r') as fp:
+            self._proofs = json.load(fp)
+            fp.close()
+
+    def test_new_s3_bucket_policy(self):
+        new_policy = self._automator._transform_bucket_policy(
+            bucket_policy=None, principal_account=PRODUCER_ACCOUNT,
+            bucket_name='s3://org-1-data'
+        )
+        self.assertEqual(self._proofs.get('New').get('Statement'), new_policy.get('Statement'))
+
+    def test_add_lf_service_role(self):
+        # contents of this policy must match the contents of s3_bucket_policy_proofs.json.NoDataMesh
+        starting_policy = {
+            "Version": "2012-10-17",
+            "Id": "Policy1632328705971",
+            "Statement": [
+                {
+                    "Sid": "1234567890",
+                    "Effect": "Allow",
+                    "Principal": {
+                        "AWS": [
+                            "600214582022"
+                        ]
+                    },
+                    "Action": [
+                        "ram:AcceptSharingInvitation"
+                    ],
+                    "Resource": [
+                        "*"
+                    ]
+                }
+            ]}
+        new_policy = self._automator._transform_bucket_policy(
+            bucket_policy=starting_policy, principal_account=PRODUCER_ACCOUNT,
+            bucket_name='s3://org-1-data'
+        )
+        self.assertEqual(self._proofs.get('NoDataMesh').get('Statement'), new_policy.get('Statement'))
+
+    def test_add_principal(self):
+        # contents of this policy must match the contents of s3_bucket_policy_proofs.json.NewPrincipal
+        starting_policy = {
+            "Version": "2012-10-17",
+            "Id": "Policy1632328705971",
+            "Statement": [
+                {
+                    "Sid": "AwsDataMeshUtilsBucketPolicyStatement-org-1-data",
+                    "Effect": "Allow",
+                    "Principal": {
+                        "AWS": [
+                            "arn:aws:iam::600214582022:role/aws-service-role/lakeformation.amazonaws.com/AWSServiceRoleForLakeFormationDataAccess"
+                        ]
+                    },
+                    "Action": [
+                        "s3:GetObject*"
+                    ],
+                    "Resource": [
+                        "arn:aws:s3:::org-1-data",
+                        "arn:aws:s3:::org-1-data/*"
+                    ]
+                }
+            ]}
+        new_policy = self._automator._transform_bucket_policy(
+            bucket_policy=starting_policy, principal_account=MESH_ACCOUNT,
+            bucket_name='s3://org-1-data'
+        )
+        self.assertEqual(self._proofs.get('NewPrincipal').get('Statement'), new_policy.get('Statement'))
+
+    def test_no_action(self):
+        # contents of this policy must match the contents of s3_bucket_policy_proofs.json.New
+        starting_policy = {
+            "Version": "2012-10-17",
+            "Id": "Policy1632328705971",
+            "Statement": [
+                {
+                    "Sid": "AwsDataMeshUtilsBucketPolicyStatement-org-1-data",
+                    "Effect": "Allow",
+                    "Principal": {
+                        "AWS": [
+                            "arn:aws:iam::600214582022:role/aws-service-role/lakeformation.amazonaws.com/AWSServiceRoleForLakeFormationDataAccess"
+                        ]
+                    },
+                    "Action": [
+                        "s3:GetObject*"
+                    ],
+                    "Resource": [
+                        "arn:aws:s3:::org-1-data",
+                        "arn:aws:s3:::org-1-data/*"
+                    ]
+                }
+            ]}
+        new_policy = self._automator._transform_bucket_policy(
+            bucket_policy=starting_policy, principal_account=PRODUCER_ACCOUNT,
+            bucket_name='s3://org-1-data'
+        )
+        self.assertEqual(self._proofs.get('New').get('Statement'), new_policy.get('Statement'))
