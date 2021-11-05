666f11a3fd65260885fb6baaa60efd657df02517
Ian Meyers <meyersi@amazon.com>
2021-11-05 11:54:17 +0000

    flipping order of producer/consumer initialization in unit tests

--- test/data_mesh_account_tests.py
+++ test/data_mesh_account_tests.py
@@ -31,23 +31,23 @@ class DataMeshAdminAccountTests(unittest.TestCase):
         self._logger.info(output)
 
     def test_setup_producer(self):
-        # enable the producer in the mesh account
-        mesh_admin = dmu.DataMeshAdmin(data_mesh_account_id=MESH_ACCOUNT, region_name=self._region,
-                                       log_level=logging.DEBUG, use_creds=self._creds.get(MESH))
-        mesh_admin.enable_account_as_producer(self._account_ids.get(PRODUCER))
-
         # in the producer account, we'll initialize the required objects
         producer_admin = dmu.DataMeshAdmin(data_mesh_account_id=MESH_ACCOUNT, region_name=self._region,
                                            log_level=logging.DEBUG, use_creds=self._creds.get(PRODUCER_ADMIN))
         producer_admin.initialize_producer_account()
 
-    def test_setup_consumer(self):
-        # enable the consumer in the mesh account
+        # enable the producer in the mesh account
         mesh_admin = dmu.DataMeshAdmin(data_mesh_account_id=MESH_ACCOUNT, region_name=self._region,
                                        log_level=logging.DEBUG, use_creds=self._creds.get(MESH))
-        mesh_admin.enable_account_as_consumer(self._account_ids.get(CONSUMER))
+        mesh_admin.enable_account_as_producer(self._account_ids.get(PRODUCER))
 
+    def test_setup_consumer(self):
         # in the consumer account, we'll initialize the required objects
         consumer_admin = dmu.DataMeshAdmin(data_mesh_account_id=MESH_ACCOUNT, region_name=self._region,
                                            log_level=logging.DEBUG, use_creds=self._creds.get(CONSUMER_ADMIN))
         consumer_admin.initialize_consumer_account()
+        
+        # enable the consumer in the mesh account
+        mesh_admin = dmu.DataMeshAdmin(data_mesh_account_id=MESH_ACCOUNT, region_name=self._region,
+                                       log_level=logging.DEBUG, use_creds=self._creds.get(MESH))
+        mesh_admin.enable_account_as_consumer(self._account_ids.get(CONSUMER))
