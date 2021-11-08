7d485638f6415a9bef3deb42fc17c31669abc6fa
Ian Meyers <meyersi@amazon.com>
2021-11-08 09:22:27 +0000

    removing hard coded mesh account (derp)

--- test/reinvent/0_setup_central_account.py
+++ test/reinvent/0_setup_central_account.py
@@ -13,9 +13,6 @@ from data_mesh_util import DataMeshAdmin as dmu
 
 warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
 
-MESH_ACCOUNT = '887210671223'
-
-
 class Step0():
     '''
     Script to configure an set of accounts as central data mesh, producer, and consumer. Mesh credentials must already
@@ -29,18 +26,18 @@ class Step0():
 
     def setup_central_account(self):
         # create the data mesh
-        mesh_admin = dmu.DataMeshAdmin(data_mesh_account_id=MESH_ACCOUNT, region_name=self._region,
+        mesh_admin = dmu.DataMeshAdmin(data_mesh_account_id=self._account_ids.get(MESH), region_name=self._region,
                                        log_level=logging.DEBUG, use_creds=self._creds.get(MESH))
         mesh_admin.initialize_mesh_account()
 
         # create the producer account
-        producer_admin = dmu.DataMeshAdmin(data_mesh_account_id=MESH_ACCOUNT, region_name=self._region,
+        producer_admin = dmu.DataMeshAdmin(data_mesh_account_id=self._account_ids.get(MESH), region_name=self._region,
                                            log_level=logging.DEBUG, use_creds=self._creds.get(PRODUCER_ADMIN))
         producer_admin.initialize_producer_account()
         mesh_admin.enable_account_as_producer(self._account_ids.get(PRODUCER))
 
         # create the consumer_account
-        consumer_admin = dmu.DataMeshAdmin(data_mesh_account_id=MESH_ACCOUNT, region_name=self._region,
+        consumer_admin = dmu.DataMeshAdmin(data_mesh_account_id=self._account_ids.get(MESH), region_name=self._region,
                                            log_level=logging.DEBUG, use_creds=self._creds.get(CONSUMER_ADMIN))
         consumer_admin.initialize_consumer_account()
         mesh_admin.enable_account_as_consumer(self._account_ids.get(CONSUMER))
