5dc546829d46d369b8c1561e848edf44c24e548e
Ian Meyers <meyersi@amazon.com>
2021-11-17 09:45:23 +0000

    new list_pending script, fixing declarations in producer

--- test/reinvent/2_5_list_pending_access_requests.py
+++ test/reinvent/2_5_list_pending_access_requests.py
@@ -16,7 +16,7 @@ from data_mesh_util.lib.SubscriberTracker import *
 warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
 
 
-class Step3():
+class List():
     '''
     Class to test the functionality of a data producer. Should be run using credentials for a principal who can assume
     the DataMeshAdminProducer role in the data mesh. Requires environment variables:
@@ -36,24 +36,10 @@ class Step3():
     def setUp(self) -> None:
         warnings.filterwarnings("ignore", category=ResourceWarning)
 
-    def grant_access_request(self, subscription_id: str, grant_permissions: list, approval_notes: str):
+    def list_pending_requests(self):
         # approve access from the producer
-        approval = self._mgr.approve_access_request(
-            request_id=subscription_id,
-            grant_permissions=grant_permissions,
-            grantable_permissions=grant_permissions,
-            decision_notes=approval_notes
-        )
-
-        return approval
+        return self._mgr.list_pending_access_requests()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--subscription_id', dest='subscription_id', required=True)
-    parser.add_argument('--grant_permissions', nargs="+", dest='grant_permissions', required=True)
-    parser.add_argument('--approval_notes', dest='approval_notes', required=True)
-
-    args = parser.parse_args()
-    print(Step3().grant_access_request(subscription_id=args.subscription_id, grant_permissions=args.grant_permissions,
-                                       approval_notes=args.approval_notes))
+    print(List().list_pending_requests())
